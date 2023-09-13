79056280b7b05757b7530a79f4f7595d0b480c35
seulgi <0_1998@naver.com>
2023-09-13 17:59:15 +0900

    feat: pocketmoney

--- Front/tailwind.config.js
+++ Front/tailwind.config.js
@@ -11,12 +11,17 @@ export default {
         gray: "#F0F0F0",
         darkgray: "#666666",
         black: "#000000",
+        // 임시 추가
+        yellow: "#F4D160",
+        sky: "#75C2F6",
       },
       fontSize: {
         s: "1.2rem",
         m: "1.6rem",
         l: "2rem",
         x: "2.4rem",
+        // 임시 추가
+        sm: "1.4rem",
       },
       fontFamily: {
         main: ["main"],
