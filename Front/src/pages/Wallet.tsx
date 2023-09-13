ccef6078dac1b9520869547eaf058e20c3e36823
이정훈 <wjdgns0631@gmail.com>
2023-09-13 17:24:05 +0900

    Feat : 카드지갑 UI

--- Front/src/pages/Wallet.tsx
+++ Front/src/pages/Wallet.tsx
@@ -1,12 +1,117 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+
+
+const Icon = ({ h = 6, w = 6 }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={`w-${w} h-${h}`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+      />
+    </svg>
+  );
+};
+
+
+const TradeList = () => {
+  return (
+    <div className="">
+      <div className="px-4 my-4">
+        <div>9월 7일</div>
+        <hr />
+      </div>
+      {/* 항목 */}
+      <div className="">
+        <TradeListItem />
+        <TradeListItem />
+        <TradeListItem />
+      </div>
+    </div>
+  );
+};
+
+const TradeListItem = () => {
+  return (<div className="flex m-5">
+          <div className="bg-gray w-[40px] h-[40px] rounded-[50%] flex items-center justify-center">
+            <Icon />
+          </div>
+          <div className="flex w-full justify-between">
+            <div className="ml-10">
+              <div>이우철 용돈</div>
+              <div>11:20</div>
+            </div>
+            <div>
+              <div className="text-main text-left">-2,500 원</div>
+              <div className="text-darkgray text-left">남은 돈 15,000 원</div>
+            </div>
+          </div>
+        </div>)
+}
 
 const Wallet = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <p>Wallet</p>
       <div className="bg-yellow-100">
         <Link to="/">Login</Link>
+      </div>
+      <div className="m-8">
+        <div>
+          <div className="p-10 text-white h-[140px] rounded-2xl [background:linear-gradient(270deg,_#1c51ad_20%,_rgba(28,_81,_173,_0.3))]">
+            <div className="ml-3">
+              <div>잔액</div>
+              <div className="text-m">99,000 원</div>
+            </div>
+            <div className="flex justify-between px-6 py-5 text-center">
+              <div>
+                <div className="bg-neutral-800 w-14 h-10">아이콘</div>
+                <div>용돈 관리</div>
+              </div>
+              <div>
+                <div className="bg-neutral-800 w-14 h-10">아이콘</div>
+                <div>충전</div>
+              </div>
+              <div>
+                <div className="bg-neutral-800 w-14 h-10">아이콘</div>
+                <div>송금</div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="flex p-6 justify-between">
+              <div className="flex gap-5">
+                <div className="rounded-[15px] w-[60px] h-[30px] text-center text-main text-m bg-light">
+                  전체
+                </div>
+                <div>입금</div>
+                <div>출금</div>
+              </div>
+              <button onClick={() => navigate("search")}>
+                <Icon />
+              </button>
+            </div>
+            <div className="bg-gray h-16 rounded-xl flex justify-between p-4 items-center">
+              <Icon />
+              <div>{"2023년 09월"}</div>
+              <Icon />
+            </div>
+            <div>
+              {/* 날짜별 */}
+              <TradeList />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
