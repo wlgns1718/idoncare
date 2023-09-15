3e76bf5fc79c00f8ce9807b48828be7d1389b199
이정훈 <wjdgns0631@gmail.com>
2023-09-15 11:55:18 +0900

    feat : 충전 금액 UI

--- Front/src/components/wallet/RechargeAccountList.tsx
+++ Front/src/components/wallet/RechargeAccountList.tsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
+import FullBtn from "../common/FullBtn";
 
 function RechargeAccountList() {
   return (
     <div>
       <div>출금 계좌</div>
-      <div>
-        <div className="w-full h-[80px] bg-gray rounded-xl flex ">
-          <div>아이콘</div>
-          <div className='flex'>
-            <div className='flex-col'>
+      <div className="">
+        <div className="w-full h-[80px] px-8 my-4 bg-gray rounded-xl flex items-center  justify-between ">
+          <div className="flex items-center">
+            <div className="mx-8">아이콘</div>
+            <div className="flex-col justify-center flex">
               <div>NH 농협</div>
               <div>12983723498732</div>
             </div>
-            <div>▼</div>
           </div>
+          <div>▼</div>
+        </div>
+        <div className="w-full h-[80px] bg-gray rounded-xl flex-col flex items-center justify-center my-4">
+          <div>충전 계좌 변경</div>
+          <div>아이콘</div>
         </div>
       </div>
+      <FullBtn buttonText="충전" buttonLink="password" />
     </div>
   );
 }
 
-export default RechargeAccountList
+export default RechargeAccountList;
