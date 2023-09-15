a5746c3919748e31599877e63f2d3be35d443f85
이정훈 <wjdgns0631@gmail.com>
2023-09-15 09:42:41 +0900

    Refair : Icon 컴포넌트

--- Front/src/components/common/BottomNav.tsx
+++ Front/src/components/common/BottomNav.tsx
@@ -2,17 +2,17 @@ import Icon from "./Icon";
 
 const BottomTaps = [
   {
-    icon: <Icon name="purchase" size="large" />,
+    icon: <Icon name="purchase" size="medium" />,
     link: "/purchase",
     text: "결제",
   },
   {
-    icon: <Icon name="home" size="large" />,
+    icon: <Icon name="home" size="medium" />,
     link: "/home",
     text: "홈",
   },
   {
-    icon: <Icon name="total" size="large" />,
+    icon: <Icon name="total" size="medium" />,
     link: "/total",
     text: "전체",
   },
@@ -21,7 +21,7 @@ const BottomTaps = [
 function BottomNav() {
   const iconWidth = "60px";
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-white rounded-t-3xl border-t-2 p-4 flex justify-evenly ">
+    <div className="fixed bottom-0 left-0 w-full bg-white rounded-t-3xl border-t-2 p-x flex justify-evenly ">
       {BottomTaps.map((item, index) => {
         return (
           <div
