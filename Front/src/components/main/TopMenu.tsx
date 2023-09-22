cc72ad04436b3dc6dc74a6998fec368769892a4a
이정훈 <wjdgns0631@gmail.com>
2023-09-22 17:22:56 +0900

    Refact : 메인 카드지갑

--- Front/src/components/main/TopMenu.tsx
+++ Front/src/components/main/TopMenu.tsx
@@ -1,24 +1,33 @@
-import TopMenuBox from '../main/TopMenuBox'
-import TopAccount from './TopAccount'
+import TopMenuBox from "../main/TopMenuBox";
+import WalletCard from "../wallet/WalletCard";
 
 function TopMenu() {
-    return (
-      <div className="bg-main rounded-b-3xl flex flex-col items-center"
-      style={{ height: '330px', borderBottomLeftRadius: '60px', borderBottomRightRadius: '60px' }}>
-          <div>
-              <img src="/icons/icon-logo-2.png" alt="logo" className="w-36 h-28 p-3 mt-2"/>
-          </div>
-
-          <TopAccount />
-          
-          <div className="flex justify-between">
-              <TopMenuBox link="/pocketMoney" bgColor="gray" text="용돈" />
-              <TopMenuBox link="/" bgColor="gray" text="미션" />
-          </div>
-  
+  return (
+    <div
+      className="bg-main rounded-b-3xl flex flex-col items-center px-6 pb-10"
+      style={{
+        borderBottomLeftRadius: "60px",
+        borderBottomRightRadius: "60px",
+      }}
+    >
+      <div>
+        <img
+          src="/icons/icon-logo-2.png"
+          alt="logo"
+          className="w-36 h-28 p-3 mt-2"
+        />
       </div>
-    )
-  }
-  
+      <div className="w-full">
+        <div className="w-full">
+          <WalletCard />
+        </div>
+        <div className="flex justify-between w-full gap-4 pb-4">
+          <TopMenuBox link="/pocketMoney" bgColor="gray" text="용돈" />
+          <TopMenuBox link="/" bgColor="gray" text="미션" />
+        </div>
+      </div>
+    </div>
+  );
+}
 
-export default TopMenu
+export default TopMenu;
--- Front/src/components/wallet/WalletCard.tsx
+++ Front/src/components/wallet/WalletCard.tsx
@@ -1,32 +1,59 @@
-import Icon from "../common/Icon";
+import Icon, { ICON_NAME } from "../common/Icon";
 import { useNavigate } from "react-router-dom";
+
+type cardButtonType = {
+  text: string;
+  icon: ICON_NAME;
+  link: string;
+};
+
+const cardButton: cardButtonType[] = [
+  {
+    link: "/pocketMoney",
+    text: "용돈관리",
+    icon: "home",
+  },
+  {
+    link: "recharge",
+    text: "충전",
+    icon: "home",
+  },
+  {
+    link: "/transfer/account",
+    text: "송금",
+    icon: "home",
+  },
+];
 
 function WalletCard() {
   const navigate = useNavigate();
   return (
-    <div className="p-10 text-white h-[140px] rounded-2xl [background:linear-gradient(270deg,_#1c51ad_20%,_rgba(28,_81,_173,_0.3))]">
-      <div className="ml-3">
-        <div>잔액</div>
-        <div className="text-m">99,000 원</div>
-      </div>
-      <div className="flex justify-between px-6 py-5 text-center">
-        <div>
-          <div className="w-10 h-10 m-auto">
-            <Icon size="small" />
-          </div>
-          <div>용돈 관리</div>
+    <div className="bg-white rounded-2xl overflow-hidden">
+      <div className="p-10 text-white h-[140px] rounded-2xl [background:linear-gradient(270deg,_#1c51ad_20%,_rgba(28,_81,_173,_0.3))]">
+        <div
+          className="ml-3"
+          onClick={() => {
+            navigate("/wallet");
+          }}
+        >
+          <div>잔액</div>
+          <div className="text-m">99,000 원</div>
         </div>
-        <div onClick={() => navigate("recharge")}>
-          <div className=" w-10 h-10 m-auto">
-            <Icon size="small" />
-          </div>
-          <div>충전</div>
-        </div>
-        <div onClick={() => navigate("/transfer/account")}>
-          <div className="w-10 h-10 m-auto">
-            <Icon size="small" />
-          </div>
-          <div>송금</div>
+        <div className="flex justify-between px-6 py-5 text-center">
+          {cardButton.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="h-10"
+                onClick={() => navigate(item.link)}
+              >
+                <div className="px-2">
+                  <Icon size="small" name={item.icon} />
+                </div>
+                <div className="w-16">{item.text}</div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
