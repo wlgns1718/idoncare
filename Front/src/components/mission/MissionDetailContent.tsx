a19ea50c5dcd16d17e530c768ac53e0881bea199
이정훈 <wjdgns0631@gmail.com>
2023-09-24 18:06:23 +0900

    Feat : 미션 디테일 화면

---
+++ Front/src/components/mission/MissionDetailContent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MissionDetailContent() {
+  return (
+    <div>
+      <div>제목부</div>
+      <div>메시지</div>
+      <div>내용</div>
+    </div>
+  )
+}
+
+export default MissionDetailContent
