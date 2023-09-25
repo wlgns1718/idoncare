677c530089790b40a253a1962ac697e4a7ecc9aa
이정훈 <wjdgns0631@gmail.com>
2023-09-26 01:00:54 +0900

    Feat : 충전 계좌 등록 요청

--- Front/src/pages/WalletRecharge.tsx
+++ Front/src/pages/WalletRecharge.tsx
@@ -2,8 +2,33 @@ import Header from "../components/common/Header";
 import RechargeInput from "../components/wallet/RechargeInput";
 import RechargeAccountList from "../components/wallet/RechargeAccountList";
 import FullBtn from "../components/common/FullBtn";
+import axios from "axios";
+import { userToken } from "../store/common/atoms";
+import { useRecoilValue } from "recoil";
+import { rechargeAccount } from "../store/wallet/atoms";
 
 function WalletRecharge() {
+  const Token = useRecoilValue(userToken);
+  const myRechargeAccount = useRecoilValue(rechargeAccount);
+
+  const rechageMoney = () => {
+    axios
+      .post(
+        `http://j9d209.p.ssafy.io:8081/api/account/charge`,
+        {
+          pinNumber: myRechargeAccount?.pinNumber,
+          money: 10000,
+          type: "CHAGE",
+        },
+        {
+          headers: { Authorization: Token as string },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
   return (
     <div>
       <Header pageTitle="계좌 충전" headerLink="back" headerType="normal" />
@@ -11,7 +36,7 @@ function WalletRecharge() {
         <RechargeInput />
         <RechargeAccountList />
       </div>
-      <FullBtn buttonText="충전" buttonLink="password" />
+      <FullBtn buttonText="충전" buttonLink="password" onClick={rechageMoney} />
     </div>
   );
 }
