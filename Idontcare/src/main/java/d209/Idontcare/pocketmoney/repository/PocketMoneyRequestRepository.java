e97c2736f3bcf206e3a24e66ab4ceab25083450a
이우철 <lwc421@gmail.com>
2023-09-27 13:39:55 +0900

    Fix: 부모가 아이의 용돈 요청목록 볼때 content 추가

--- Idontcare/src/main/java/d209/Idontcare/pocketmoney/repository/PocketMoneyRequestRepository.java
+++ Idontcare/src/main/java/d209/Idontcare/pocketmoney/repository/PocketMoneyRequestRepository.java
@@ -11,13 +11,13 @@ import java.util.List;
 public interface PocketMoneyRequestRepository extends JpaRepository<PocketMoneyRequest, Long> {
   
   @Query("select p.pocketMoneyRequestId as pocketMoneyRequestId, p.parent.userId as parentId, p.parent.name as parentName, p.child.userId as childId, p.child.name as childName, " +
-      "p.type as type, p.amount as amount, p.createdAt as createdAt, p.cancelDate as cancelDate" +
+      "p.type as type, p.amount as amount, p.createdAt as createdAt, p.cancelDate as cancelDate, p.content as content" +
       " from PocketMoneyRequest p where p.parent = :parent")
   List<Tuple> findAllByParent(User parent);
   
   
   @Query("select p.pocketMoneyRequestId as pocketMoneyRequestId, p.parent.userId as parentId, p.parent.name as parentName, p.child.userId as childId, p.child.name as childName, " +
-      "p.type as type, p.amount as amount, p.createdAt as createdAt, p.cancelDate as cancelDate" +
+      "p.type as type, p.amount as amount, p.createdAt as createdAt, p.cancelDate as cancelDate, p.content as content" +
       " from PocketMoneyRequest p where p.child = :child")
   List<Tuple> findAllByChild(User child);
 }
