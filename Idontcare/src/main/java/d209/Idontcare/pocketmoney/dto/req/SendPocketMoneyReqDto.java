af3e27d8aec48bd29672ed7ee83ff8e4a74912c9
이우철 <lwc421@gmail.com>
2023-09-29 12:02:48 +0900

    Fix: 일회성 용돈 comment 추가

--- Idontcare/src/main/java/d209/Idontcare/pocketmoney/dto/req/SendPocketMoneyReqDto.java
+++ Idontcare/src/main/java/d209/Idontcare/pocketmoney/dto/req/SendPocketMoneyReqDto.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
@@ -17,4 +18,8 @@ public class SendPocketMoneyReqDto {
   @Schema(description = "얼마", example = "1000")
   @NotNull(message = "용돈이 입력되어야합니다") @Positive(message = "용돈은 0보다 커야합니다")
   private Long amount;
+  
+  @Schema(description = "코멘트", example = "잘 쓰렴")
+  @NotEmpty(message = "하고싶은 말을 입력해야 합니다")
+  private String comment;
 }
