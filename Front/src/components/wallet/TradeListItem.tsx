72aca7f4cf07056fa932c5f3cbcf96a8cfcae4ec
이정훈 <wjdgns0631@gmail.com>
2023-10-05 15:06:03 +0900

    Fix : 거래 시간 형식 변경

--- Front/src/components/wallet/TradeListItem.tsx
+++ Front/src/components/wallet/TradeListItem.tsx
@@ -15,16 +15,18 @@ function TradeListItem({item}: TradeItemProps) {
       <div className="flex w-full justify-between">
         <div className="ml-10">
           <div>{item.content}</div>
-          <div>{item.localTime}</div>
+          <div>{item.localTime.substring(0, 5)}</div>
         </div>
         <div className="flex-col flex items-end">
-          <div className={`text-left ${item.cashFlow == "DEPOSIT"?"text-rose-600":"text-main"}`}>
+          <div
+            className={`text-left ${
+              item.cashFlow == "DEPOSIT" ? "text-rose-600" : "text-main"
+            }`}
+          >
             {item.cashFlow == "DEPOSIT" ? "+" : "-"}
             {item.amount} 원
           </div>
-          <div className="text-darkgray text-left">
-            잔액 {item.balance} 원
-          </div>
+          <div className="text-darkgray text-left">잔액 {item.balance} 원</div>
         </div>
       </div>
     </div>
