8bec99ce347361f835d7b70898621fc3c54678cd
LWC421 <lwc421@gmail.com>
2023-10-05 21:22:09 +0900

    Style: MyPage에서 기정의된 이미지만 나오도록 변경

--- Front/src/components/common/Profile.tsx
+++ Front/src/components/common/Profile.tsx
@@ -39,22 +39,23 @@ const ProfileSize = {
   large: 120,
 };
 
-const randomNumber = Math.floor(Math.random() * 5);
-
 const Profile = ({
   profileName,
   profileImage,
   size = "large",
   type = "image",
 }: ProfileProps) => {
+  const randomNumber = Math.floor(Math.random() * 5);
+
   return (
     <div className="flex flex-col items-center justify-around w-full">
       <img
         className={`w-[${ProfileSize[size]}px] h-[${ProfileSize[size]}px] rounded-full`}
         src={
-          type == "image"
-            ? profileImage
-            : profileImages[type == "PARENT" ? randomNumber : randomNumber + 5]
+          // type == "image"
+          //   ? profileImage
+          //   : profileImages[type == "PARENT" ? randomNumber : randomNumber + 5]
+          profileImages[randomNumber]
         }
         alt="프로필 이미지"
       />
