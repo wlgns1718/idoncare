ece0f3303c98fe727bf21bee7c636fac87a6e12e
seulgi <0_1998@naver.com>
2023-10-06 13:08:15 +0900

    Fix: Alert -> Modal 로 수정

--- Front/src/pages/KidRegist.tsx
+++ Front/src/pages/KidRegist.tsx
@@ -9,11 +9,14 @@ import { userToken } from "../store/common/selectors";
 import AxiosHeader from "../apis/axios/AxiosHeader";
 import { baseUrl } from "../apis/url/baseUrl";
 import BottomNav from "../components/common/BottomNav";
+import Modal from "../components/common/Modal";
 
 const KidRegist: React.FC = () => {
   const token = useRecoilValue(userToken);
   const [childPhoneNumber, setChildPhoneNumber] = useState("");
   const navigate = useNavigate();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
 
   const handleChildPhoneNumberSubmit = async () => {
     try {
@@ -26,13 +29,15 @@ const KidRegist: React.FC = () => {
       console.log(response.data);
       console.log(childPhoneNumber);
 
-      alert("등록되었습니다.");
-      navigate("/");
+      setModalMessage("요청이 전송되었습니다.");
+      setModalIsOpen(true);
+      // navigate("/");
     } catch (error) {
       console.error(error);
       console.log(childPhoneNumber);
 
-      alert("오류가 발생했습니다.");
+      setModalMessage("오류가 발생하였습니다.");
+      setModalIsOpen(true);
     }
   };
 
@@ -45,9 +50,27 @@ const KidRegist: React.FC = () => {
           자녀의 전화번호를 입력해주세요.
         </div>
         <KidPhoneForm onPhoneNumberChange={setChildPhoneNumber} />
+
+        {/* Move the modal component here */}
+        {modalIsOpen && (
+          <Modal>
+            <div className="text-m m-16">{modalMessage}</div>
+            <FullBtn
+              buttonText="확인"
+              onClick={() => {
+                setModalIsOpen(false);
+                if (modalMessage === "자녀 요청이 완료되었습니다.")
+                  navigate("/");
+              }}
+            />
+          </Modal>
+        )}
       </div>
 
+      {/* Button to submit phone number */}
       <FullBtn buttonText="등록" onClick={handleChildPhoneNumberSubmit} />
+
+      {/* Bottom navigation */}
       <BottomNav />
     </div>
   );
