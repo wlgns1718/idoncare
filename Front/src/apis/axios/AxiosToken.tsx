5d059eb15af8508ebf3bce7eab513586e369cb81
wptjd6141@naver.com <wptjd6141@naver.com>
2023-10-12 04:02:48 +0900

    Test: Axios 처리 후 토큰 갱신 테스트

--- Front/src/apis/axios/AxiosToken.tsx
+++ Front/src/apis/axios/AxiosToken.tsx
@@ -1,13 +1,18 @@
 import { useRecoilState } from "recoil";
 import { userData } from "../../store/common/atoms";
+import { useEffect } from "react";
 
 interface AxiosTokenProps {
   token: string;
 }
 
 const AxiosToken = ({ token }: AxiosTokenProps) => {
+  console.log(token);
   const [userInfo, setUserInfo] = useRecoilState(userData);
   setUserInfo({ ...userInfo, accessToken: token });
+  useEffect(() => {
+    console.log(userInfo);
+  }, [userInfo]);
 };
 
 export default AxiosToken;
