962eae9c979dca2ffeb4fed333c4e008ca8b79da
wptjd6141@naver.com <wptjd6141@naver.com>
2023-10-13 01:24:23 +0900

    Feat: AccessToken 처리

--- Front/src/apis/axios/AxiosToken.tsx
+++ Front/src/apis/axios/AxiosToken.tsx
@@ -1,19 +1,18 @@
 import { useRecoilState } from "recoil";
 import { userData } from "../../store/common/atoms";
-import { useEffect } from "react";
 
 interface AxiosTokenProps {
   token: string;
 }
 
 const AxiosToken = ({ token }: AxiosTokenProps) => {
-  console.log("토큰: " + token);
   const [userInfo, setUserInfo] = useRecoilState(userData);
+
+  if (token === undefined) {
+    token = userInfo.accessToken!;
+  }
+
   setUserInfo({ ...userInfo, accessToken: token });
-  useEffect(() => {
-    console.log("유저 정보");
-    console.log(userInfo);
-  }, [userInfo]);
 };
 
 export default AxiosToken;
