ae22f6a273b89a2cd2b7b5668aa06752b84e09cb
이정훈 <wjdgns0631@gmail.com>
2023-10-13 11:37:26 +0900

    fix : 은행 이미지 주소 images 추가

--- Front/src/components/wallet/RechargeAccountList.tsx
+++ Front/src/components/wallet/RechargeAccountList.tsx
@@ -44,7 +44,7 @@ export const RechargeAccountComponent = () => {
   const handleRemoveAccount = () => {
     axios
       .delete(baseUrl + `api/account/`, {
-        headers: { Authorization: ("Bearer " + Token) as string },
+        headers: { Authorization: Token as string },
         data: {
           bankCode: myRechargeAccount?.bankCode,
           realAccountId: myRechargeAccount?.realAccountId,
@@ -66,7 +66,7 @@ export const RechargeAccountComponent = () => {
         <div className="w-[40px] mr-6">
           <img
             className=""
-            src={`https://j9d209.p.ssafy.io:9082/${myRechargeAccount?.bankName}.png`}
+            src={`https://j9d209.p.ssafy.io:9082/images/${myRechargeAccount?.bankName}.png`}
           ></img>
         </div>
         <div className="flex flex-col justify-center text-t">
