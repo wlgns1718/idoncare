15597039db55794b2d741c257db5e371a1f4d941
이정훈 <wjdgns0631@gmail.com>
2023-10-13 10:51:43 +0900

    fix : 출금 계좌 아이콘

--- Front/src/components/wallet/RechargeAccountList.tsx
+++ Front/src/components/wallet/RechargeAccountList.tsx
@@ -66,7 +66,7 @@ export const RechargeAccountComponent = () => {
         <div className="w-[40px] mr-6">
           <img
             className=""
-            src={`https://port-0-openbankapi-iciy2almk8xusg.sel5.cloudtype.app/images/${myRechargeAccount?.bankName}.png`}
+            src={`https://j9d209.p.ssafy.io:9082${myRechargeAccount?.bankName}`}
           ></img>
         </div>
         <div className="flex flex-col justify-center text-t">
