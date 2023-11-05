326e7ad04a99e6326ce20c8c08a71158ed634fb6
성연석 <abcd9351@naver.com>
2023-11-05 18:27:22 +0900

    Feat: 초기 설정 완료

--- virtualaccount/virtualaccount/src/test/java/com/example/virtualaccount/VirtualaccountApplicationTests.java
+++ virtualaccount/virtualaccount/src/test/java/com/example/virtualaccount/VirtualaccountApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ssafital.idoncare;
+package com.example.virtualaccount;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApigatewayApplicationTests {
+class VirtualaccountApplicationTests {
 
 	@Test
 	void contextLoads() {
